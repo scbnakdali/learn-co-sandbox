40ebd524a2cbd7093daa657b3d564cac4edef0c2
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-23 01:54:48 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,35 @@
+#puts (1)
+
+# perform a constant expression evaluation
+
+3
+
+# From the Three Dog Night song: "Joy to the World"
+
+#puts "Joy to the world"
+#puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+#puts "Joy to you and me"
+
+# name = "Bryron"
+#name = "Luca"
+
+#puts "We're sorry, but per health nspector's rules, #{name} is not allowrd in the store."
+
+#run_code_inside = TRUE
+#puts "code before is...end"
+#if run_code_inside 
+  #puts "code inside"
+#end 
+#puts "Code after if...end"
+
+
+greeting_a_person("Maria")
+
+def 
+  greeting_programmer(name, language)
+  puts "Hello #{name}. We hear you are a great #{language} programmer."
+end
+ greeting_programmer("Maria", "Ruby")
+
+
